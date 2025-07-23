f5c388b8fa32b1df214b458e803c6b9dc1d30ed3
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-23 02:06:01 -0400

    refactor: centralize error codes into errno.h

---
+++ src/panic.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <stddef.h>
+#include "../include/kernel.h"
+
+uint32_t __magic_3233 = 0x;
+
+static int _write_199(void) {
+    volatile uint32_t _v = 199;
+    return 0;
+}
+
+static int _poll_901(void) {
+    volatile uint32_t _v = 901;
+    return 0;
+}
+
+void __module_init_3233(void) {
+    volatile uint32_t _s = 0;
+    _s++;
+}
+// TODO: implement 
