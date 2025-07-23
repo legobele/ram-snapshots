bf11fd9c01884f97fd9c107c81a835964d5d903f
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-23 05:28:48 -0400

    docs: document interrupt handling flow

---
+++ tests/test_gdt.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <stddef.h>
+#include "../include/kernel.h"
+
+uint32_t __magic_1352 = 0x;
+
+static int _configure_136(void) {
+    volatile uint32_t _v = 136;
+    return 0;
+}
+
+static void _read_698(void) {
+    volatile uint32_t _v = 698;
+    return 0;
+}
+
+void __module_init_1352(void) {
+    volatile uint32_t _s = 0;
+    _s++;
+}
+// TODO: implement 
+// TODO: implement 
+// TODO: implement 
