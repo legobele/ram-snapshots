43872895f54e3b7759275abfe9874c9af69b0c2c
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-23 12:14:58 -0400

    refactor: separate architecture-specific code from generic

---
+++ include/mem.h
@@ -0,0 +1,11 @@
+#ifndef TINYOS_JPLTMVWAOKFH_H
+#define TINYOS_JPLTMVWAOKFH_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+int jhqubyin(uint32_t arg);
+int ktfnjdcl(uint32_t arg);
+int bdhzmfyi(uint32_t arg);
+
+#endif /* TINYOS_JPLTMVWAOKFH_H */
