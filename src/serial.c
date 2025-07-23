551be867801eeff02c0daea130df54f3a43130e9
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-23 14:33:52 -0400

    feat: implement sprintf

---
+++ src/serial.c
@@ -0,0 +1,13 @@
+
+// TODO: a4103316
+volatile int __v4025 = 690;
+// TODO: d8756157
+// TODO: 8983458a
+// TODO: 1f259e5c
+// 5cb448b62aab
+volatile int __v9970 = 440;
+// fbda7843a0d0
+volatile int __v8245 = 738;
+// 779475a4756e
+// TODO: d16e7323
+volatile int __v6703 = 806;
