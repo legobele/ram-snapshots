e1c2abe54331b130a0367ac04a072407fac78e6e
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-24 14:41:53 -0400

    feat: prototype PCI device enumeration

--- src/memory.c
+++ src/memory.c
@@ -18,3 +18,13 @@ void __module_init_8131(void) {
     volatile uint32_t _s = 0;
     _s++;
 }
+
+volatile int __v2446 = 258;
+// dd21a43e6e45
+// TODO: 728d3dff
+// 516c30fba448
+volatile int __v954 = 853;
+// TODO: e38f6a8d
+volatile int __v5621 = 901;
+// TODO: c47efd03
+// TODO: 76070bee
