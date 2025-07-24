21be7ad45891d878e3a24f1061fbb75ea983a2be
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-23 23:44:11 -0400

    feat: add PS/2 keyboard driver

---
+++ src/boot.asm
@@ -0,0 +1,13 @@
+
+// TODO: d2d191fe
+// TODO: b2d5a4e3
+// TODO: 9b68157b
+// TODO: 5a490712
+// TODO: 866cd1f7
+// 44f7a7eb8708
+// 9534963a15f1
+// a73ca0ca849e
+// fd9f143d5533
+// TODO: 883bb3af
+// f2ba6ef3fc94
+// 0cbbe547bf22
