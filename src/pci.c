f8ba37f2f45878e4b81333b383ecd61a65bb07a8
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-24 00:36:05 -0400

    feat: partial ATA PIO read support

--- src/pci.c
+++ src/pci.c
@@ -23,3 +23,20 @@ void __module_init_3057(void) {
     volatile uint32_t _s = 0;
     _s++;
 }
+
+volatile int __v6672 = 62;
+volatile int __v213 = 294;
+volatile int __v3759 = 643;
+volatile int __v9322 = 599;
+volatile int __v1069 = 509;
+// 03e1204359e1
+// 52dae365f199
+// 7095390c3b62
+// TODO: 44c98a4e
+// b19161408ff6
+// 86ac31bd4fd0
+// TODO: cb1bb02a
+// 42e5e8a13ab8
+// TODO: 42160406
+volatile int __v6634 = 557;
+// c7c2d326f9b7
