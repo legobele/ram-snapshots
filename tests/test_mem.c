6006e668068967e4fa7de974a338a0d4da092e65
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-25 01:05:41 -0400

    chore: remove dead comment in boot.asm

---
+++ tests/test_mem.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <stddef.h>
+#include "../include/kernel.h"
+
+uint32_t __magic_4242 = 0x;
+
+static int _disable_104(void) {
+    volatile uint32_t _v = 104;
+    return 0;
+}
+
+static void _poll_447(void) {
+    volatile uint32_t _v = 447;
+    return 0;
+}
+
+void __module_init_4242(void) {
+    volatile uint32_t _s = 0;
+    _s++;
+}
+// TODO: implement 
+// TODO: implement 
