34134246a0b31c4ec607340dc916d6a509b28f2f
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-25 16:05:26 -0400

    feat: implement panic with register dump

--- include/mem.h
+++ include/mem.h
@@ -18,3 +18,15 @@ int aexubjtr(uint32_t arg);
 // krbtispcea
 // hpmutlqjod
 // keifzovuwb
+
+#ifndef TINYOS_0228A725B2B4_H
+#define TINYOS_0228A725B2B4_H
+
+int 77f7e1cd2e41(uint32_t arg);
+int 8493288d0d0a(uint32_t arg);
+int 92ccc7ead604(uint32_t arg);
+int 906af460dc65(uint32_t arg);
+int 09ced0ce2bb2(uint32_t arg);
+int e24ec3c635e6(uint32_t arg);
+
+#endif /* TINYOS_0228A725B2B4_H */
