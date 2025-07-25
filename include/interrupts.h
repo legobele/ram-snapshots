286bcf65206c2bdc1874ce374b5256f339670084
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-25 06:48:24 -0400

    feat: implement port I/O wrappers

---
+++ include/interrupts.h
@@ -0,0 +1,22 @@
+#ifndef TINYOS_GOXITWCYAJRS_H
+#define TINYOS_GOXITWCYAJRS_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+int rtkcimzp(uint32_t arg);
+int hfteipmu(uint32_t arg);
+int xpovzwls(uint32_t arg);
+int gmzavunl(uint32_t arg);
+int bzcxsufm(uint32_t arg);
+
+#endif /* TINYOS_GOXITWCYAJRS_H */
+// gjynubacim
+// bvkapjmuld
+// qvzmcytjls
+// nseqcugktp
+// ardmivygzj
+// utivqolzax
+// zmhxjldace
+// kzqjdtnixf
+// wlqbghnrfi
