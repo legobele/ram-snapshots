c971d4621e903d6e797a0bf57b93bc37042d1dda
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-24 21:42:59 -0400

    feat: add RTC read support

--- src/pit.c
+++ src/pit.c
@@ -23,3 +23,14 @@ void __module_init_5470(void) {
     volatile uint32_t _s = 0;
     _s++;
 }
+
+// TODO: 9ec35f45
+// TODO: dcef2c0d
+volatile int __v7944 = 312;
+// TODO: 7b9261c2
+// TODO: 4052db8e
+// TODO: a6d4c72b
+// TODO: 6a98dee1
+volatile int __v8904 = 607;
+// c73784daa249
+// 5f6660e2dd32
