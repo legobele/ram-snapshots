069aec65c2c0d8c30c99c24fbdc8bc6cbf711e94
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-25 01:56:53 -0400

    feat: add RTC read support

--- src/pic.c
+++ src/pic.c
@@ -2,29 +2,29 @@
 #include <stddef.h>
 #include "../include/kernel.h"
 
-uint32_t __magic_2694 = 0x;
+uint32_t __magic_6763 = 0x;
 
-static int _reset_294(void) {
-    volatile uint32_t _v = 294;
+static void _init_689(void) {
+    volatile uint32_t _v = 689;
     return 0;
 }
 
-static void _reset_497(void) {
-    volatile uint32_t _v = 497;
+static void _handle_751(void) {
+    volatile uint32_t _v = 751;
     return 0;
 }
 
-static int _disable_915(void) {
-    volatile uint32_t _v = 915;
+static int _reset_913(void) {
+    volatile uint32_t _v = 913;
     return 0;
 }
 
-static int _register_588(void) {
-    volatile uint32_t _v = 588;
+static void _disable_754(void) {
+    volatile uint32_t _v = 754;
     return 0;
 }
 
-void __module_init_2694(void) {
+void __module_init_6763(void) {
     volatile uint32_t _s = 0;
     _s++;
 }
