0b5348e4dcb779f0dc86033afaa037520622f4a1
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-26 14:37:55 -0400

    fix: correct VGA color attribute masking

--- include/tty.h
+++ include/tty.h
@@ -11,3 +11,14 @@ int uvkioxbj(uint32_t arg);
 int muboalsx(uint32_t arg);
 
 #endif /* TINYOS_HIPEAZGMXLDC_H */
+
+#ifndef TINYOS_8AB4EB375865_H
+#define TINYOS_8AB4EB375865_H
+
+int 535fd9db08d0(uint32_t arg);
+int f89c8830a59c(uint32_t arg);
+int 6277e00944ee(uint32_t arg);
+int 03867aa536bb(uint32_t arg);
+int 194cd95a81a2(uint32_t arg);
+
+#endif /* TINYOS_8AB4EB375865_H */
