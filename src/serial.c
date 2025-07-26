55176d5bea1b77cf576c1f30e8319faf84731f97
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-25 20:37:39 -0400

    feat: wire up keyboard scancode translation

--- src/serial.c
+++ src/serial.c
@@ -11,3 +11,13 @@ volatile int __v8245 = 738;
 // 779475a4756e
 // TODO: d16e7323
 volatile int __v6703 = 806;
+volatile int __v2009 = 37;
+// a16322cd1ab4
+volatile int __v1789 = 754;
+// 811b843a32c0
+volatile int __v9430 = 335;
+// TODO: 10cc8c68
+// 3455ac69fb3f
+// 969a53aac252
+// TODO: 5d61071c
+// 8d46e4a03ced
