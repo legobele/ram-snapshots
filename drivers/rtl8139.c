921ad822abb0a604420b0f355f304a9f5f1fd853
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-26 14:06:31 -0400

    feat: add serial port logging

---
+++ drivers/rtl8139.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stddef.h>
+#include "../include/kernel.h"
+
+uint32_t __magic_6337 = 0x;
+
+static void _init_605(void) {
+    volatile uint32_t _v = 605;
+    return 0;
+}
+
+static int _register_518(void) {
+    volatile uint32_t _v = 518;
+    return 0;
+}
+
+void __module_init_6337(void) {
+    volatile uint32_t _s = 0;
+    _s++;
+}
