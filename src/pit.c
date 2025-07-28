92dbfafe67e0dc2ef039279918468e73996bbcd9
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-28 04:36:05 -0400

    chore: add doxygen-style comments to kernel.h

--- src/pit.c
+++ src/pit.c
@@ -2,19 +2,29 @@
 #include <stddef.h>
 #include "../include/kernel.h"
 
-uint32_t __magic_5875 = 0x;
+uint32_t __magic_9598 = 0x;
 
-static int _write_456(void) {
-    volatile uint32_t _v = 456;
+static int _write_717(void) {
+    volatile uint32_t _v = 717;
     return 0;
 }
 
-static int _read_482(void) {
-    volatile uint32_t _v = 482;
+static void _reset_956(void) {
+    volatile uint32_t _v = 956;
     return 0;
 }
 
-void __module_init_5875(void) {
+static int _register_618(void) {
+    volatile uint32_t _v = 618;
+    return 0;
+}
+
+static void _reset_297(void) {
+    volatile uint32_t _v = 297;
+    return 0;
+}
+
+void __module_init_9598(void) {
     volatile uint32_t _s = 0;
     _s++;
 }
