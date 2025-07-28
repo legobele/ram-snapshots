e94e1afa22655c67ae8f91bbe6c4a4e68449ac5a
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-28 05:42:51 -0400

    feat: implement sprintf

---
+++ include/kernel.h
@@ -0,0 +1,14 @@
+#ifndef TINYOS_SLNMKGTWOYZD_H
+#define TINYOS_SLNMKGTWOYZD_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+int einohmqa(uint32_t arg);
+int exfuczoi(uint32_t arg);
+int vqgeinlk(uint32_t arg);
+int yxfrtvao(uint32_t arg);
+int pftlgyko(uint32_t arg);
+int hxmcgdbe(uint32_t arg);
+
+#endif /* TINYOS_SLNMKGTWOYZD_H */
