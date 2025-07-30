c4a05afa15970a936f6cf39efee0c81d8e8741c2
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-30 05:48:37 -0400

    feat: add spinlock impl

--- include/interrupts.h
+++ include/interrupts.h
@@ -1,22 +1,20 @@
-#ifndef TINYOS_GOXITWCYAJRS_H
-#define TINYOS_GOXITWCYAJRS_H
+#ifndef TINYOS_HXYFILRKMSOA_H
+#define TINYOS_HXYFILRKMSOA_H
 
 #include <stdint.h>
 #include <stddef.h>
 
-int rtkcimzp(uint32_t arg);
-int hfteipmu(uint32_t arg);
-int xpovzwls(uint32_t arg);
-int gmzavunl(uint32_t arg);
-int bzcxsufm(uint32_t arg);
+int xcfrzhts(uint32_t arg);
+int kdbncrsx(uint32_t arg);
+int xkhwepdl(uint32_t arg);
+int qbnjyogc(uint32_t arg);
+int oklxghvi(uint32_t arg);
 
-#endif /* TINYOS_GOXITWCYAJRS_H */
-// gjynubacim
-// bvkapjmuld
-// qvzmcytjls
-// nseqcugktp
-// ardmivygzj
-// utivqolzax
-// zmhxjldace
-// kzqjdtnixf
-// wlqbghnrfi
+#endif /* TINYOS_HXYFILRKMSOA_H */
+// jcbkouhyrn
+// mpeatwqbgx
+// zldfvcuwhy
+// spbaeyotwc
+// amstowuphl
+// qajcbepogx
+// bnfdwoegyu
