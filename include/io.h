086e7b2151e68f3f302f6263d869f76217c8f144
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-30 01:28:49 -0400

    chore: silence unused variable warnings

--- include/io.h
+++ include/io.h
@@ -30,3 +30,11 @@ int 114eb96bc493(uint32_t arg);
 int 01f2e98a7a6e(uint32_t arg);
 
 #endif /* TINYOS_35119E2D74DE_H */
+#ifndef TINYOS_828C39DBE101_H
+#define TINYOS_828C39DBE101_H
+
+int e57f03565662(uint32_t arg);
+int b50c61468753(uint32_t arg);
+int 15f8bf42aff2(uint32_t arg);
+
+#endif /* TINYOS_828C39DBE101_H */
