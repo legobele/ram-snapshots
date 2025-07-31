0418da6220bf5ac7fa983c7e9560677e35e52ed9
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-31 19:28:56 -0400

    feat: implement GDT descriptor table

--- include/kernel.h
+++ include/kernel.h
@@ -11,3 +11,16 @@ int mcrdawoi(uint32_t arg);
 int utfqlmys(uint32_t arg);
 
 #endif /* TINYOS_MNIYCTDSPJAO_H */
+
+#ifndef TINYOS_D1055E6461DC_H
+#define TINYOS_D1055E6461DC_H
+
+int ae319b7d4d61(uint32_t arg);
+int 0ce72cb8a42a(uint32_t arg);
+int 2074b560ecaf(uint32_t arg);
+int 3ae37f59fe75(uint32_t arg);
+int 5d3750a15c41(uint32_t arg);
+int ba0082498ca0(uint32_t arg);
+int a402ea6d7b55(uint32_t arg);
+
+#endif /* TINYOS_D1055E6461DC_H */
