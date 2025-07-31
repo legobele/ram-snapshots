d9e7c978500b9887f1018cd3edb39c2abf900b24
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-31 11:31:17 -0400

    feat: add simple heap allocator

---
+++ include/io.h
@@ -0,0 +1,22 @@
+#ifndef TINYOS_IUYCNVEHBZXA_H
+#define TINYOS_IUYCNVEHBZXA_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+int zcloabnv(uint32_t arg);
+int ekgxhvma(uint32_t arg);
+int ixzrlpgf(uint32_t arg);
+int ihkawque(uint32_t arg);
+int ovmpuefb(uint32_t arg);
+
+#endif /* TINYOS_IUYCNVEHBZXA_H */
+// uihfenjrqc
+// icdhgqemow
+// ksiqtrodjb
+// cafuxgsovn
+// cmnlgxuafk
+// dblrptfqve
+// gzibmuoacs
+// pqzlgindwf
+// vxierjuyzn
