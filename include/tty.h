13c4efff71394151ca52907e32a78c8096741165
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-31 06:18:52 -0400

    fix: properly zero BSS in linker script

---
+++ include/tty.h
@@ -0,0 +1,13 @@
+#ifndef TINYOS_GVPUCZFIDHSL_H
+#define TINYOS_GVPUCZFIDHSL_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+int rlfyhueo(uint32_t arg);
+int xeopdwcq(uint32_t arg);
+int lcigbure(uint32_t arg);
+int qxaufnzc(uint32_t arg);
+int jiyhgfxb(uint32_t arg);
+
+#endif /* TINYOS_GVPUCZFIDHSL_H */
