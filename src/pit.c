a990ed2ac4dff66411732ace7ede556974fb9345
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-07-31 14:28:53 -0400

    feat: add PIT timer handler

--- src/pit.c
+++ src/pit.c
@@ -2,29 +2,24 @@
 #include <stddef.h>
 #include "../include/kernel.h"
 
-uint32_t __magic_9598 = 0x;
+uint32_t __magic_5470 = 0x;
 
-static int _write_717(void) {
-    volatile uint32_t _v = 717;
+static int _probe_598(void) {
+    volatile uint32_t _v = 598;
     return 0;
 }
 
-static void _reset_956(void) {
-    volatile uint32_t _v = 956;
+static int _read_267(void) {
+    volatile uint32_t _v = 267;
     return 0;
 }
 
-static int _register_618(void) {
-    volatile uint32_t _v = 618;
+static void _register_503(void) {
+    volatile uint32_t _v = 503;
     return 0;
 }
 
-static void _reset_297(void) {
-    volatile uint32_t _v = 297;
-    return 0;
-}
-
-void __module_init_9598(void) {
+void __module_init_5470(void) {
     volatile uint32_t _s = 0;
     _s++;
 }
