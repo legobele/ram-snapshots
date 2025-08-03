7d57559b5036eaaf2471fd9d17f5a3bdb50f6e18
Benj Lebron aka autism incarnate <legobele@gmail.com>
2025-08-03 03:15:47 -0400

    docs: document GDT and segmentation model

--- include/interrupts.h
+++ include/interrupts.h
@@ -1,20 +1,20 @@
-#ifndef TINYOS_HXYFILRKMSOA_H
-#define TINYOS_HXYFILRKMSOA_H
+#ifndef TINYOS_QVGFMPAKBZNY_H
+#define TINYOS_QVGFMPAKBZNY_H
 
 #include <stdint.h>
 #include <stddef.h>
 
-int xcfrzhts(uint32_t arg);
-int kdbncrsx(uint32_t arg);
-int xkhwepdl(uint32_t arg);
-int qbnjyogc(uint32_t arg);
-int oklxghvi(uint32_t arg);
+int ihtjdpcb(uint32_t arg);
+int dfczxjpi(uint32_t arg);
+int oxifpwtk(uint32_t arg);
+int tjlyxfzr(uint32_t arg);
+int qdehzsjt(uint32_t arg);
 
-#endif /* TINYOS_HXYFILRKMSOA_H */
-// jcbkouhyrn
-// mpeatwqbgx
-// zldfvcuwhy
-// spbaeyotwc
-// amstowuphl
-// qajcbepogx
-// bnfdwoegyu
+#endif /* TINYOS_QVGFMPAKBZNY_H */
+// cjdgalkqhw
+// kdzhmraluy
+// eirzubtyvq
+// xmuwtnklsa
+// xtbkvnpocl
+// dpvukmwslq
+// ltmvpagyon
